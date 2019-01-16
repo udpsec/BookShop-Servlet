b913630108f2448a1e1520727dad4a10e150c10e
udpsec <udpsec@users.noreply.github.com>
2019-01-16 12:41:53 +0800

    用户登录Demo

---
+++ src/main/java/com/inofd/controller/LoginServlet.java
@@ -0,0 +1,40 @@
+package com.inofd.controller;
+
+import com.inofd.service.UserService;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserService userService = new UserService();
+        List<String> info = new ArrayList<String>();
+        HttpSession session = request.getSession();
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (userService.checkUser(username, password)) {
+            response.sendRedirect("main.jsp");
+        } else {
+            info.add("用户名或密码错误");
+            session.setAttribute("info",info);
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2019/1/15
+  Time: 15:58
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>%
 <html>
+<head>
+    <title>Title</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<c:forEach var="info" items="${sessionScope.info}" step="1">
+    <span>info</span>
+</c:forEach>
+<form action="LoginServlet" method="post">
+
+    Username: <input type="text" name="username">
+    <br>
+    Password: <input type="password" name="password">
+    <br>
+    <input type="submit" value="Login">
+</form>
+</body>
 </body>
 </html>
