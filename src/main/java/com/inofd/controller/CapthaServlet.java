b59eb18993617dcd91d1e79c15e14e1df04d9cd7
udpsec <udpsec@users.noreply.github.com>
2019-01-17 16:41:54 +0800

    update

---
+++ src/main/java/com/inofd/controller/CapthaServlet.java
@@ -0,0 +1,50 @@
+package com.inofd.controller;
+
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Random;
+
+@WebServlet(name = "CapthaServlet", urlPatterns = "/CapthaServlet")
+public class CapthaServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int width = 120;
+        int height = 30;
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics graphics = bufferedImage.getGraphics();
+        graphics.setColor(Color.gray);
+        graphics.fillRect(0, 0, width, height);
+        graphics.setColor(Color.blue);
+        Random random = new Random();
+        for (int i = 0; i < 4; i++) {
+            int x1 = random.nextInt(width);
+            int y1 = random.nextInt(height);
+
+            int x2 = random.nextInt(width);
+            int y2 = random.nextInt(height);
+            graphics.drawLine(x1, y1, x2, y2);
+        }
+        int left = 5;
+        graphics.setColor(Color.yellow);
+        for (int i = 0; i < 4; i++) {
+            int r = random.nextInt(10);
+            graphics.drawString(r + "", left + i * 20, 20);
+        }
+        response.setContentType("image/jpeg");
+        ImageIO.write(bufferedImage, "JPEG", response.getOutputStream());
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,10 +7,18 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<% String APP_PATH = pageContext.getServletContext().getContextPath();%>
 <html>
 <head>
     <title>Title</title>
 </head>
+<script type="text/javascript">
+    function imgrestore() {
+        var imgtag = document.getElementById("verfi");
+        // 加上Date时间可以换验证码否则都是请求同一个验证码
+        imgtag.src = "/ServletProject_war_exploded/CapthaServlet?" + new Date().getTime();
+    }
+</script>
 <body>
 ${sessionScope.info}
 <form action="LoginServlet" method="post">
@@ -19,6 +27,9 @@ ${sessionScope.info}
     <br>
     Password: <input type="password" name="password">
     <br>
+    验证码:<input type="text">
+    <img src="/ServletProject_war_exploded/CapthaServlet" align="verification" id="verfi">
+    <a href="javascript:imgrestore();">看不清？换个验证码</a><br>
     <input type="submit" value="Login">
 </form>
 </body>
